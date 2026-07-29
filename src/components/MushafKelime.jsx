ec576fef719a7878e025cf099cdb9666f45aabc1
hosgoer <hoschgor@gmail.com>
2026-07-29 13:33:00 +0300

    Mushaf verisi düzenlemeleri devam ediyor.

--- src/components/MushafKelime.jsx
+++ src/components/MushafKelime.jsx
@@ -240,7 +240,7 @@ export default function MushafKelime({
                   {c}
                   <span style={{
                     position: 'absolute',
-                    bottom: arapcaFont.toLowerCase().includes('scheherazade') ? '-0.15em' : '-0.55em',
+                    bottom: arapcaFont.toLowerCase().includes('scheherazade') ? '-0.02em' : '-0.5em',
                     left: arapcaFont.toLowerCase().includes('scheherazade') ? '+0.33em' : '+0.2em',
                     transform: 'none',
                     fontSize: `${yaziBoyutu}px`,
